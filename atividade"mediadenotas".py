0920f0ade251edbd06c64bc890564c32ff2884d5
matheuspcsantos <mcognolatto@gmail.com>
2025-05-23 12:50:07 -0300

    Create atividade"mediadenotas".py

---
+++ atividade"mediadenotas".py
@@ -0,0 +1,13 @@
+n1 = float(input("Digite sua primeira nota: ")) 
+n2 = float(input("Digite sua segunta nota: ")) 
+n3 = float(input("Digite sua terceira nota: ")) 
+n4 = float(input("Digite sua quarta nota: ")) 
+
+media = (n1 + n2 + n3 + n4)/4 
+
+print("Minha média final é ", media) 
+
+if media >= 5: 
+    print("Aprovado(a)") 
+else:
+    print("Reprovado(a)")
