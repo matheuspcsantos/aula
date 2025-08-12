7ff0aaf08d883ecee3bf4e39e0285c326d256ef1
matheuspcsantos <mcognolatto@gmail.com>
2025-08-12 08:56:33 -0300

    Create salario.py

---
+++ salario.py
@@ -0,0 +1,8 @@
+salario = float(input("Digite o valor do salario: ")) 
+essencial = salario * 0.50
+prioridades = salario * 0.15 
+estilo = salario * 0.35
+
+print(f"O valor que será investido em essenciais como, água, comida, luz, aluguel entre outros, o valor é: {essencial}") 
+print(f"O valor que será investido em prioridades como, investimento para aposentadoria, conta de emergencia,investimentos, entre outros,o valor é: {prioridades}") 
+print(f"O valor que será investido em estilo de vida como, academia, cinema, delivery, entre outras, o valor é: {estilo}")
