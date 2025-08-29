793e1246d1f8c48b487972c475b948280e4e7d98
matheuspcsantos <mcognolatto@gmail.com>
2025-08-29 13:30:58 -0300

    Create criandoclasse.py

---
+++ criandoclasse.py
@@ -0,0 +1,54 @@
+class Pessoa:
+    nome = ""
+    idade = 0 
+    cidade = ""
+    profissão = ""
+
+#Primeira Pessoa 
+p1 = Pessoa()
+p1.nome = "Matheus Pizzo"
+p1.idade = 16
+p1.cidade = "São Paulo"
+p1.profissão = "Estudante"
+
+#Segunda Pessoa 
+p2 = Pessoa() 
+p2.nome = "Vergil" 
+p2.idade = 40 
+p2.cidade = "Red Grave City"
+p2.profissão = "em busca de mais poder"
+#Terceira Pessoa 
+p3 = Pessoa () 
+p3.nome = "Big Boss" 
+p3.idade = 35 
+p3.cidade = "nenhuma" 
+p3.profissão = "Espião"
+
+#Quarta Pessoa 
+p4 = Pessoa () 
+p4.nome = "Dante" 
+p4.idade = 40 
+p4.cidade = "Red Grave City" 
+p4.profissão = "Caçador de demônios" 
+
+#Quinta Pessoa 
+p5 = Pessoa () 
+p5.nome = "Gwyn Lord of Cinder" 
+p5.idade = 112 
+p5.cidade = "Anor Londo" 
+p5.profissão = "Rei"
+
+#Imprimindo dados 
+print("Pessoa 1:", p1.nome, "-", p1.idade, "anos: ", "Profissão: ", p1.profissão, "Cidade: ", p1.cidade) 
+
+#Imprimindo dados 
+print("Pessoa 2:", p2.nome, "-", p2.idade, "anos: ",  "Profissão: ", p2.profissão, "Cidade: ", p2.cidade)
+
+#Imprimindo dados 
+print("Pessoa 3:", p3.nome, "-", p3.idade, "anos: ",  "Profissão: ", p3.profissão, "Cidade: ", p3.cidade) 
+
+#Imprimindo dados 
+print("Pessoa 4:", p4.nome, "-", p4.idade, "anos: ",  "Profissão: ", p4.profissão, "Cidade: ", p4.cidade)
+
+#Imprimindo dados 
+print("Pessoa 5:", p5.nome, "-", p5.idade, "anos: ",  "Profissão: ", p5.profissão, "Cidade: ", p5.cidade) 
