d9cbb2da95ec376cf582a6868d0f6cb0509fe0f4
matheuspcsantos <mcognolatto@gmail.com>
2025-08-29 13:34:34 -0300

    Create verificaçãodesenha.py

---
+++ verificaçãodesenha.py
@@ -0,0 +1,11 @@
+# Senha correta
+senha_correta = "1234"
+
+# Entrada do usuário
+senha = input("Digite a senha: ")
+
+# Verificação
+if senha == senha_correta:
+    print("Senha correta!")
+else:
+    print("Senha incorreta!")
