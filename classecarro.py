bcfca711693cabd592d32e4d87dc918311abc7c7
matheuspcsantos <mcognolatto@gmail.com>
2025-08-29 13:57:50 -0300

    Create classecarro.py

---
+++ classecarro.py
@@ -0,0 +1,62 @@
+class Carro: 
+    marca = "" 
+    modelo = "" 
+    ano = 0
+
+#Primeiro carro 
+p1 = Carro() 
+p1.marca = "Nissan"
+p1.modelo = "Skyline GT-R" 
+p1.ano = 1999 
+
+#Segundo carro
+p2 = Carro() 
+p2.marca = "Mitsubishi"
+p2.modelo = "Lancer" 
+p2.ano = 2019 
+
+#Terceiro carro 
+p3 = Carro() 
+p3.marca = "Porsche"
+p3.modelo = "911" 
+p3.ano = 2016 
+
+#Quarto carro
+p4 = Carro() 
+p4.marca = "Koenigsseg"
+p4.modelo = "Jesko" 
+p4.ano = 2016 
+
+#Quinto carro
+p5 = Carro() 
+p5.marca = "Koenigsseg"
+p5.modelo = "Venom" 
+p5.ano = 2017 
+
+#Sexto carro
+p6 = Carro() 
+p6.marca = "Bugatti"
+p6.modelo = "chiron" 
+p6.ano = 2019
+
+#Setimo carro
+p7 = Carro() 
+p7.marca = "Fiat"
+p7.modelo = "Uno" 
+p7.ano = 1998 
+
+#Oitavo carro
+p8 = Carro() 
+p8.marca = "Chevrolet"
+p8.modelo = "Corsa" 
+p8.ano = 2008
+
+#Imprimindo dados
+print("Carro 1: ", p1.marca, "-", p1.modelo, p1.ano ) 
+print("Carro 2: ", p2.marca, "-", p2.modelo, p2.ano ) 
+print("Carro 3: ", p3.marca, "-", p3.modelo, p3.ano ) 
+print("Carro 4: ", p4.marca, "-", p4.modelo, p4.ano ) 
+print("Carro 5: ", p5.marca, "-", p5.modelo, p5.ano ) 
+print("Carro 6: ", p6.marca, "-", p6.modelo, p6.ano ) 
+print("Carro 7: ", p7.marca, "-", p7.modelo, p7.ano ) 
+print("Carro 8: ", p8.marca, "-", p8.modelo, p8.ano )
