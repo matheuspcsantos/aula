041d49946c0c45fbb3492af822fccb59d0e4935f
matheuspcsantos <mcognolatto@gmail.com>
2025-08-29 13:31:38 -0300

    Create classefilme.py

---
+++ classefilme.py
@@ -0,0 +1,76 @@
+class Filme: 
+    título = "" 
+    ano = 0 
+    gênero = "" 
+    classificação = ""
+
+#Primeiro filme 
+p1 = Filme() 
+p1.título = "Taxi Driver" 
+p1.ano = 1976 
+p1.gênero = "Drama, Ação, Suspense e Noir" 
+p1.classificação = "14 anos" 
+
+#Segundo filme
+p2 = Filme() 
+p2.título = "Clube da Luta" 
+p2.ano = 1999 
+p2.gênero = "Ação, Suspense, Drama" 
+p2.classificação = "18 anos" 
+
+#Terceiro filme
+p3 = Filme() 
+p3.título = "O Show de Truman" 
+p3.ano = 1998
+p3.gênero = "Comédia, Drama, Ficção científica" 
+p3.classificação = "12 anos" 
+
+#Quarto filme
+p4 = Filme() 
+p4.título = "Se beber, não case!" 
+p4.ano = 2009
+p4.gênero = "Comédia, Aventura, Drama" 
+p4.classificação = "14 anos" 
+
+#Quinto filme
+p5 = Filme() 
+p5.título = "Corra que a Polícia Vem Aí!" 
+p5.ano = 2025
+p5.gênero = "Comédia, Aventura, Polícial" 
+p5.classificação = "14 anos" 
+
+#Sexto filme 
+p6 = Filme() 
+p6.título = "Kung-Fusão" 
+p6.ano = 2004
+p6.gênero = "Comédia, Ação, Artes marciais" 
+p6.classificação = "14 anos" 
+
+#Setimo filme 
+p7 = Filme() 
+p7.título = "Interestelar" 
+p7.ano = 2014
+p7.gênero = "Ficção científica, Drama, Ação" 
+p7.classificação = "10 anos" 
+
+
+#Imprimindo dados 
+print("Filme 1: ", p1.título, "-", p1.ano, "ano de lançamento","gênero: ", p1.gênero, "classificação: ", p1.classificação) 
+
+#Imprimindo dados 
+print("Filme 2: ", p2.título, "-", p2.ano, "ano de lançamento","gênero: ", p2.gênero, "classificação: ", p2.classificação)
+
+#Imprimindo dados 
+print("Filme 3: ", p3.título, "-", p3.ano, "ano de lançamento","gênero: ", p3.gênero, "classificação: ", p3.classificação) 
+
+#Imprimindo dados 
+print("Filme 4: ", p4.título, "-", p4.ano, "ano de lançamento","gênero: ", p4.gênero, "classificação: ", p4.classificação) 
+
+#Imprimindo dados 
+print("Filme 5: ", p5.título, "-", p5.ano, "ano de lançamento","gênero: ", p5.gênero, "classificação: ", p5.classificação) 
+
+#Imprimindo dados 
+print("Filme 6: ", p6.título, "-", p6.ano, "ano de lançamento","gênero: ", p6.gênero, "classificação: ", p6.classificação) 
+
+#Imprimindo dados 
+print("Filme 7: ", p7.título, "-", p7.ano, "ano de lançamento","gênero: ", p7.gênero, "classificação: ", p7.classificação)
