ca6077617f6a3207c36d7fabc3ac056000797754
matheuspcsantos <mcognolatto@gmail.com>
2025-09-03 09:41:35 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,5 @@
+print("BEm vindo a calculadora de soma") 
+primeiro_numero = int(input("Digite seu primeiro numero: ")) 
+segundo_numero = int(print("Digite seu segundo numero: ")) 
+
+print(f'a soma do numero {primeiro_numero} + {segundo_numero}: {primeiro_numero + segundo_numero}') 
