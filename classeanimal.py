21644c059fc5b32fe10aca2162bb17dbea8f876c
matheuspcsantos <mcognolatto@gmail.com>
2025-09-03 09:42:13 -0300

    Create classeanimal.py

---
+++ classeanimal.py
@@ -0,0 +1,76 @@
+class Animal:
+    nome = ""
+    espécie = ""
+    idade = ""
+
+# Primeiro Animal
+p1 = Animal()
+p1.nome = "Ronaldinho"
+p1.espécie = "Gato"
+p1.idade = "4"
+
+# Segundo Animal
+p2 = Animal()
+p2.nome = "venom"
+p2.espécie = "Pantera Negra"
+p2.idade = "6"
+
+# Terceiro Animal
+p3 = Animal()
+p3.nome = "Robin"
+p3.espécie = "Cavalo"
+p3.idade = "10"
+
+# Quarta Animal
+p4 = Animal()
+p4.nome = "Kai"
+p4.espécie = "Cobra"
+p4.idade = "9"
+
+# Quinta Animal
+p5 = Animal()
+p5.nome = "Pó"
+p5.espécie = "Panda"
+p5.idade = "5"
+
+# Sexto Animal
+p6 = Animal()
+p6.nome = "Zé perninha"
+p6.espécie = "Coelho"
+p6.idade = "2"
+
+# Setimo Animal
+p7 = Animal()
+p7.nome = "Bob"
+p7.espécie = "papagaio"
+p7.idade = "7"
+
+# Oitavo Animal
+p8 = Animal()
+p8.nome = "Robson"
+p8.espécie = "Gorila"
+p8.idade = "12"
+
+# Nono Animal
+p9 = Animal()
+p9.nome = "Beto"
+p9.espécie = "Sapo"
+p9.idade = "3"
+
+# Decimo Animal
+p10 = Animal()
+p10.nome = "sujeirinha"
+p10.espécie = "Rato"
+p10.idade = "30"
+
+
+print("Animal 1: ",p1.nome, "-", p1.espécie, p1.idade )
+print("Animal 2: ",p2.nome, "-", p2.espécie, p2.idade )
+print("Animal 3: ",p3.nome, "-", p3.espécie, p3.idade )
+print("Animal 4: ",p4.nome, "-", p4.espécie, p4.idade )
+print("Animal 5: ",p5.nome, "-", p5.espécie, p5.idade )
+print("Animal 6: ",p6.nome, "-", p6.espécie, p6.idade )
+print("Animal 7: ",p7.nome, "-", p7.espécie, p7.idade )
+print("Animal 8: ",p8.nome, "-", p8.espécie, p8.idade )
+print("Animal 9: ",p9.nome, "-", p9.espécie, p9.idade )
+print("Animal 10: ",p10.nome, "-",p10.espécie, p10.idade )
