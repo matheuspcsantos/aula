79c92c2e2067dff70427193f1b4d1396364805f8
matheuspcsantos <mcognolatto@gmail.com>
2025-09-03 09:41:00 -0300

    Create operacao_com_texto.py

---
+++ operacao_com_texto.py
@@ -0,0 +1,17 @@
+nome = "Matheus" 
+sobrenome = "Pizzo"
+
+print(nome, sobrenome) 
+print(nome + " " + sobrenome) 
+print(f'{nome} e {sobrenome}') 
+#---------------------------------------------------------------
+#: eu sou <nome de vocês> e seu imc é <calculo do imc> 
+#---------------------------------------------------------------
+
+altura = 1.84 
+peso = 99 
+imc = peso/altura**2 
+nome = "Matheus Pizzo" 
+
+print(f'Eu sou {nome} e seu imc é {round{imc}}') 
+#---------------------------------------------------------------
