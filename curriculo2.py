76121e7d7a1f0417b3c381e322c09af4bf8d350b
matheuspcsantos <mcognolatto@gmail.com>
2025-09-09 08:41:08 -0300

    Create curriculo2.py

---
+++ curriculo2.py
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css">
+    <title>Currículo</title>
+    <style>
+        .tema-escuro{
+            background-color: black;
+            color: white;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+
+        .container{
+            max-width: 900px;
+            margin:0 auto;
+            padding: 2rem;
+        }
+
+        .cabecalho{
+            text-align: center;
+            margin-bottom: 2rem;
+        }
+
+        .cartao{
+            background-color: darkred;
+            border-radius: 8px;
+            padding: 1rem;
+            margin-bottom: 1rem;
+        }
+    </style>
+</head>
+<body class="tema-escuro">
+    <div class="container">
+        <div class="cabecalho">
+        <section class="cartao">
+        <h1>Matheus Pizzo</h1>
+        <p>Estudante Ensino Médio Integrado com o Técnico de Desenvolvimento de Sistemas</p>
+        <p>E-mail: mcognolatto@gmai.com | Telefone: 11 94258-0648</p> 
+        <p>LinkedIn: www.linkedin.com/in/matheus-pizzo-cognolatto-santos-87517a313</p>
+    </div>
+        <h2>Resumo</h2>
+        <p>Sou aluno do ensino médio e estou em busca da minha primeira oportunidade profissional. 
+        Tenho um forte interesse na área de TI, que é a carreira que pretendo seguir. 
+        Possuo nível intermediário em inglês (leitura, escrita e escuta)
+        e estou continuamente aprimorando meus conhecimentos na língua. 
+        Gostaria de iniciar minha jornada no mercado de trabalho, com o objetivo de aprimorar 
+        habilidades técnicas e comportamentais em um ambiente profissional. 
+        Busco oportunidades no nível de aprendiz.</p>
+    </section>
+    <section class="cartao">
+        <h2>Habilidades</h2>
+        <p>Linguagens de Programação:</p>
+        <div>
+            <i class="devicon-html5-plain icones-habilidade"></i>  
+            <i class="devicon-python-plain icones-habilidade"></i>
+        </div>
+        <p>Ferramentas: Github e VS Code</p>
+    </section>
+        <section class="cartao">
+            
+        </section>
+        <h2>Educação</h2>
+        <p><b>Técnico em Desenvolvimento de Sistemas</b>
+            - Escola Estadual Dom Miguel de Cervantes y Saavedra (2025 - o momento)
+            Previsão de conclusão: Dezembro 2026</p> 
+            <p><b>Cursos Complementares</b>
+            - CNA Ponte Rasa Inglês (2018 - 2025)
+        - Prepara Cursos Ponte Rasa Informática (2025 - 2026)</p> 
+    </section>
+        <section class="cartao">
+        <h2>Projetos</h2>
+        <p><b>Portifólio Pessoal: </b> Site com HTML, CSS e JavaScript <a href="file:///C:/Users/MatheusPizzoCognolat/Desktop/HTML5/aula.html/cozinheiro.html">Clique aqui</a></p>
+    </section>
+
+    <footer>
+        <p>2025 Matheus Pizzo Todos os direitos reservados</p>
+        <p>Encontre mais projwtos no meu GitHub</p>
+    </footer>
+</body>
+</html>
