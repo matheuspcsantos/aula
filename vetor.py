034d1e4935d5610e4babf59d45e92256044a98a7
matheuspcsantos <mcognolatto@gmail.com>
2025-10-03 13:31:34 -0300

    Create vetor.py

---
+++ vetor.py
@@ -0,0 +1,11 @@
+#cores = ["vermelho", "roxo", "azul", "verde", "amarelo"] 
+
+#print("As cores são", cores) 
+
+frutas= ["maçã", "pera", "uva", "pessego", "banana"] 
+
+print("Primeira fruta: ", frutas[0]) 
+print("Ultima fruta: ", frutas[4]) 
+
+frutas[2] ="morango" 
+print("Novo vetor:", frutas) 
