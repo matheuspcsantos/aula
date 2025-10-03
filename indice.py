25be07dc2cb0dd0ee5d4ef565f399f39800cb655
matheuspcsantos <mcognolatto@gmail.com>
2025-10-03 13:32:09 -0300

    Create indice.py

---
+++ indice.py
@@ -0,0 +1,5 @@
+comidas = ["pizza", "sushi", "hambúrguer", "açaí", "lasanha", "hot-dog", "churasco"]
+
+indice = int(input("digite um número de 0 a 6:")) 
+
+print("você escolheu: ", comidas[indice])
